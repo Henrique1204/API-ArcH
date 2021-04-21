c0ab1dc76fb46a6323b726c0a2af43566b931091
Paulo <pauloh16.pdr@gmail.com>
2021-04-20 22:47:53 -0300

    Implementar consulta ao banco de dados no controller cliente_get

--- src/controllers/clientes_get.controller.js
+++ src/controllers/clientes_get.controller.js
@@ -1,6 +1,15 @@
+const { select } = require('../repositories/clientes.repositorie');
+
 module.exports = async (req, res) => {
     try {
-        res.status(200).send({ mensagem: 'Controller Clientes Get!' });
+        const { _nome, _email } = req.query;
+        let consulta;
+
+        if (_nome) consulta = await select('nome', _nome);
+        else if (_email) consulta = await select('email', _email);
+        else consulta = await select(null);
+
+        res.status(200).send(consulta.resposta);
     } catch (erro) {
         console.log(erro);
     }
