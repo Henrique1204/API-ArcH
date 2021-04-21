7f768c8950a588a66812f87cb166f90b77b6b6c6
Paulo <pauloh16.pdr@gmail.com>
2021-04-20 23:13:51 -0300

    Implementar insercao de dados no banco pelo controller clientes_post

--- src/controllers/clientes_post.controller.js
+++ src/controllers/clientes_post.controller.js
@@ -1,7 +1,27 @@
+const ExceptionAPI = require('../util/ExceptionAPI');
+const { select, insert } = require('../repositories/clientes.repositorie');
+
 module.exports = async (req, res) => {
     try {
-        res.status(201).send({ mensagem: 'Controller Clientes Post!' });
+        const { nome, email, cidade, estado } = req.body;
+
+        if (!nome || !email || !cidade || !estado) throw new ExceptionAPI(400);
+
+        const consultaSelect = await select('email', email);
+        if (consultaSelect.resposta.length !== 0) throw new ExceptionAPI(422);
+
+        const { ok, resposta } = await insert(req.body);
+        if (!ok) throw new ExceptionAPI(null, resposta);
+
+        return res.status(201).send(resposta);
     } catch (erro) {
-        console.log(erro);
+        if (erro.tipo === 'API') {
+            const { cod, mensagem, erroSQL } = erro;
+
+            if (erroSQL) return res.status(cod).send({ status: 'Falha', mensagem, erroSQL });
+            return res.status(cod).send({ status: 'Falha', mensagem });
+        }
+
+        return res.status(500).send({ status: 'Falha', mensagem: erro.message });
     }
 };
